c13bf18ee87ed2f9fa78bb2dc6013ca3f31f0850
Danny-create-code <da.svacek@gmail.com>
2024-02-12 15:13:45 +0100

    made a basic fish animation

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pygame.init()
 WIDTH, HEIGHT = 640, 480
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-#color
+# color
 black = (0, 0, 0)
 
 #basic setting
@@ -18,37 +18,45 @@ clock = pygame.time.Clock()
 # fish
 fish_img = pygame.image.load("images/fish.png")
 fish_rect = fish_img.get_rect()
-fish_rect.center = (320, 420)
+fish_rect.center = (320, 400)
+fliped_fish = pygame.transform.flip(fish_img, True, False)
 
-#background
+
+# background
 bg_img = pygame.image.load("images/background.jpg")
 bg_rect = bg_img.get_rect()
 bg_rect.center = (200 ,200)
 
-
+# main cycle
 run = True
 while run:
+    
+
+    # update screen fill with black color
+    SCREEN.fill(black)
+
+    # background
+    SCREEN.blit(bg_img, bg_rect)
+    
     # keys get pressed
     keys = pygame.key.get_pressed()
-    
+
+    # fish movement
+    if keys[pygame.K_LEFT] and fish_rect.centerx >= 50:
+        SCREEN.blit(fish_img, fish_rect)
+        fish_rect.centerx -= distance
+
+    if keys[pygame.K_RIGHT] and fish_rect.centerx <= 590: 
+        SCREEN.blit(fliped_fish, fish_rect)
+        fish_rect.centerx += distance
+    else:
+        SCREEN.blit(fish_img, fish_rect) 
+
     # quit game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             sys.exit()
-
-    #fish movement
-    if keys[pygame.K_LEFT] and fish_rect.centerx >= 50:
-        fish_rect.centerx -= distance
-    if keys[pygame.K_RIGHT] and fish_rect.centerx <= 590:
-        fish_rect.centerx += distance
-        
-    # update screen fill with black color
-    SCREEN.fill(black)
-
-    # images
-    SCREEN.blit(bg_img, bg_rect)
-    SCREEN.blit(fish_img, fish_rect)
     
 
     # slowdown cycle
