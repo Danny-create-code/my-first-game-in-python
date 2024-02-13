a88cfbf01daf551efbe51a18e962fa4d0146a8eb
Danny-create-code <da.svacek@gmail.com>
2024-02-13 14:20:31 +0100

    add images with bugs and life points and  make a basic movment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 
@@ -21,11 +22,27 @@ fish_rect = fish_img.get_rect()
 fish_rect.center = (320, 400)
 fliped_fish = pygame.transform.flip(fish_img, True, False)
 
+# heart
+heart_img = pygame.image.load("images/life.png")
+heart_rect = heart_img.get_rect()
+heart_rect.centerx = (random.randint(50, 590))
+heart_rect.centery = 20
+
+# bugs
+bug_one_img = pygame.image.load("images/bug_one.png")
+bug_one_rect = bug_one_img.get_rect()
+bug_one_rect.centerx = (random.randint(50, 590))
+bug_one_rect.centery = 20
+
+bug_two_img = pygame.image.load("images/bug_two.png")
+bug_two_rect = bug_one_img.get_rect()
+bug_two_rect.centerx = (random.randint(50, 590))
+bug_two_rect.centery = 20
 
 # background
 bg_img = pygame.image.load("images/background.jpg")
 bg_rect = bg_img.get_rect()
-bg_rect.center = (200 ,200)
+bg_rect.center = (100 ,200)
 
 # main cycle
 run = True
@@ -37,9 +54,27 @@ while run:
 
     # background
     SCREEN.blit(bg_img, bg_rect)
+
+    # heart
+    SCREEN.blit(heart_img, heart_rect)
+
+    # bugs
+    SCREEN.blit(bug_one_img, bug_one_rect)
+    SCREEN.blit(bug_two_img, bug_two_rect)
     
     # keys get pressed
     keys = pygame.key.get_pressed()
+
+    # heart movement
+    if heart_rect.centery <= 600:
+        heart_rect.centery += 5
+
+    # bugs movement
+    if bug_one_rect.centery <= 600:
+        bug_one_rect.centery += 5
+
+    if bug_two_rect.centery <= 600:
+        bug_two_rect.centery += 5
 
     # fish movement
     if keys[pygame.K_LEFT] and fish_rect.centerx >= 50:
