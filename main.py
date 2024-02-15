43e05e2f70cdc784fb9160c1ebce9353762d1605
Danny-create-code <da.svacek@gmail.com>
2024-02-15 12:25:37 +0100

    added score, healt point bar and collision

--- main.py
+++ main.py
@@ -10,11 +10,25 @@ SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # color
 black = (0, 0, 0)
+white = (255, 255, 255)
 
-#basic setting
-distance = 5
+# basic setting
+distance = 8
 FPS = 60
 clock = pygame.time.Clock()
+score_point = 0
+life_point = 3
+
+# font setting
+my_font = pygame.font.SysFont("Kokila", 50)
+
+
+
+
+# healt
+life_img = pygame.image.load("images/life.png")
+life_rect = life_img.get_rect()
+life_rect.center = (80, 20)
 
 # fish
 fish_img = pygame.image.load("images/fish.png")
@@ -22,27 +36,71 @@ fish_rect = fish_img.get_rect()
 fish_rect.center = (320, 400)
 fliped_fish = pygame.transform.flip(fish_img, True, False)
 
-# heart
+# heart bar
 heart_img = pygame.image.load("images/life.png")
 heart_rect = heart_img.get_rect()
 heart_rect.centerx = (random.randint(50, 590))
-heart_rect.centery = 20
+heart_rect.centery = (random.randint(-10000, -5000))
+
 
 # bugs
 bug_one_img = pygame.image.load("images/bug_one.png")
 bug_one_rect = bug_one_img.get_rect()
 bug_one_rect.centerx = (random.randint(50, 590))
-bug_one_rect.centery = 20
+bug_one_rect.centery = (random.randint(-45, -20))
 
 bug_two_img = pygame.image.load("images/bug_two.png")
 bug_two_rect = bug_one_img.get_rect()
 bug_two_rect.centerx = (random.randint(50, 590))
-bug_two_rect.centery = 20
-
+bug_two_rect.centery = (random.randint(-50, -30))
 # background
 bg_img = pygame.image.load("images/background.jpg")
 bg_rect = bg_img.get_rect()
 bg_rect.center = (100 ,200)
+
+def falling_life_point():
+    global life_point
+    # heart movement
+    if heart_rect.centery <= 600 :
+        heart_rect.centery += random.randint(1, 7)
+    if heart_rect.centery >=599:
+        heart_rect.centerx = (random.randint(50, 590))
+        heart_rect.centery = 20
+    
+    if heart_rect.colliderect(fish_rect):
+        heart_rect.centerx = (random.randint(50, 590))
+        heart_rect.centery = 20
+        life_point += 1
+
+def falling_bugs():
+    global life_point
+    global score_point
+    # bugs movement
+    if bug_one_rect.centery <= 600:
+        bug_one_rect.centery += random.randint(1, 7)
+    if bug_one_rect.centery >=599:
+        life_point -= 1
+        bug_one_rect.centerx = (random.randint(50, 590))
+        bug_one_rect.centery = 20
+
+    if bug_two_rect.centery <= 600:
+        bug_two_rect.centery += random.randint(1, 7)
+    if bug_two_rect.centery >=599:
+        life_point -= 1
+        bug_two_rect.centerx = (random.randint(50, 590))
+        bug_two_rect.centery = 20
+
+    # buggs collision
+    if fish_rect.colliderect(bug_one_rect):
+        bug_one_rect.centerx = (random.randint(50, 590))
+        bug_one_rect.centery = 20
+        score_point += 1
+
+    if fish_rect.colliderect(bug_two_rect):
+        bug_two_rect.centerx = (random.randint(50, 590))
+        bug_two_rect.centery = 20
+        score_point += 1
+
 
 # main cycle
 run = True
@@ -55,27 +113,33 @@ while run:
     # background
     SCREEN.blit(bg_img, bg_rect)
 
-    # heart
-    SCREEN.blit(heart_img, heart_rect)
+    score = my_font.render(f"{score_point}", True, black)
+    score_rect = score.get_rect()
+    score_rect.center = (30, 22)
 
-    # bugs
+    life_count = my_font.render(f"{life_point}", True, black)
+    life_count_rect = life_count.get_rect()
+    life_count_rect.center = (110, 22)
+
+    # images
+    SCREEN.blit(heart_img, heart_rect)
     SCREEN.blit(bug_one_img, bug_one_rect)
     SCREEN.blit(bug_two_img, bug_two_rect)
+    SCREEN.blit(life_img, life_rect)
+
+    # text
+    SCREEN.blit(score, score_rect)
+    SCREEN.blit(life_count, life_count_rect)
     
     # keys get pressed
     keys = pygame.key.get_pressed()
 
-    # heart movement
-    if heart_rect.centery <= 600:
-        heart_rect.centery += 5
+    
+    falling_life_point()
 
-    # bugs movement
-    if bug_one_rect.centery <= 600:
-        bug_one_rect.centery += 5
-
-    if bug_two_rect.centery <= 600:
-        bug_two_rect.centery += 5
-
+    falling_bugs()
+    
+   
     # fish movement
     if keys[pygame.K_LEFT] and fish_rect.centerx >= 50:
         SCREEN.blit(fish_img, fish_rect)
