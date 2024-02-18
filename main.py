b597caa27735426713da5e407841365968e3f2af
Danny-create-code <da.svacek@gmail.com>
2024-02-18 14:51:14 +0100

    added sounds and background music

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ pygame.init()
 # window
 WIDTH, HEIGHT = 640, 480
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Nom, nom, nom")
 
 # color
 black = (0, 0, 0)
-white = (255, 255, 255)
 
 # basic setting
 distance = 8
@@ -19,11 +19,23 @@ clock = pygame.time.Clock()
 score_point = 0
 life_point = 3
 
+current_life_point = life_point
+current_score_point = score_point
+
+# sounds
+pygame.mixer.music.load("sounds/bg_sound.mp3")
+pygame.mixer.music.play(-1,0.0)
+
+
+
+
 # font setting
 my_font = pygame.font.SysFont("Kokila", 50)
 
 
-
+end_text = my_font.render("END GAME", True, 100)
+end_text_rect = end_text.get_rect()
+end_text_rect.center = 320, 240
 
 # healt
 life_img = pygame.image.load("images/life.png")
@@ -40,7 +52,7 @@ fliped_fish = pygame.transform.flip(fish_img, True, False)
 heart_img = pygame.image.load("images/life.png")
 heart_rect = heart_img.get_rect()
 heart_rect.centerx = (random.randint(50, 590))
-heart_rect.centery = (random.randint(-10000, -5000))
+heart_rect.centery = (random.randint(-45, -20))
 
 
 # bugs
@@ -53,53 +65,103 @@ bug_two_img = pygame.image.load("images/bug_two.png")
 bug_two_rect = bug_one_img.get_rect()
 bug_two_rect.centerx = (random.randint(50, 590))
 bug_two_rect.centery = (random.randint(-50, -30))
+
 # background
 bg_img = pygame.image.load("images/background.jpg")
 bg_rect = bg_img.get_rect()
 bg_rect.center = (100 ,200)
 
 def falling_life_point():
-    global life_point
-    # heart movement
-    if heart_rect.centery <= 600 :
-        heart_rect.centery += random.randint(1, 7)
-    if heart_rect.centery >=599:
+    global current_life_point
+    # heart's movement 
+    if heart_rect.centery <= 10000 :
+        heart_rect.centery += random.randint(5, 15)
+    if heart_rect.centery >= 9999:
         heart_rect.centerx = (random.randint(50, 590))
         heart_rect.centery = 20
     
+    # heart's collision
     if heart_rect.colliderect(fish_rect):
         heart_rect.centerx = (random.randint(50, 590))
         heart_rect.centery = 20
-        life_point += 1
+        current_life_point += 1
+        take_life_sound = pygame.mixer.Sound("sounds/nom.mp3")
+        take_life_sound.set_volume(0.4)
+        take_life_sound.play()
 
 def falling_bugs():
-    global life_point
-    global score_point
-    # bugs movement
+    global current_life_point
+    global current_score_point
+    # bugs's movement
     if bug_one_rect.centery <= 600:
         bug_one_rect.centery += random.randint(1, 7)
     if bug_one_rect.centery >=599:
-        life_point -= 1
+        current_life_point -= 1
         bug_one_rect.centerx = (random.randint(50, 590))
         bug_one_rect.centery = 20
 
     if bug_two_rect.centery <= 600:
         bug_two_rect.centery += random.randint(1, 7)
     if bug_two_rect.centery >=599:
-        life_point -= 1
+        current_life_point -= 1
         bug_two_rect.centerx = (random.randint(50, 590))
         bug_two_rect.centery = 20
 
-    # buggs collision
+    # buggs's collision
     if fish_rect.colliderect(bug_one_rect):
         bug_one_rect.centerx = (random.randint(50, 590))
         bug_one_rect.centery = 20
-        score_point += 1
+        current_score_point += 1
+        take_life_sound = pygame.mixer.Sound("sounds/nom_two.mp3")
+        take_life_sound.set_volume(0.4)
+        take_life_sound.play()
+        
 
     if fish_rect.colliderect(bug_two_rect):
         bug_two_rect.centerx = (random.randint(50, 590))
         bug_two_rect.centery = 20
-        score_point += 1
+        current_score_point += 1
+        take_life_sound = pygame.mixer.Sound("sounds/nom_three.mp3")
+        take_life_sound.set_volume(0.4)
+        take_life_sound.play()
+
+
+def end_game():
+    global current_life_point
+    global current_score_point
+    global score_point
+    global life_point
+    if current_life_point == 0:
+        # end game text
+        
+        end_sound = pygame.mixer.Sound("sounds/game_over.mp3")
+        end_sound.set_volume(0.4)
+        end_sound.play()
+        
+        SCREEN.blit(end_text, end_text_rect)
+        pygame.display.update()
+
+        pause = True
+        while pause:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    pygame.time.wait(1000)
+                    pygame.mixer.music.play(-1,0.0)
+                    
+                    current_score_point = score_point
+                    current_life_point = life_point
+                    bug_one_rect.centery = 20
+                    bug_two_rect.centery = 20
+                    pause = False
+                elif event.type == pygame.QUIT:
+                    pygame.quit()
+        
+        
+
+
+        
+
+        
 
 
 # main cycle
@@ -113,11 +175,13 @@ while run:
     # background
     SCREEN.blit(bg_img, bg_rect)
 
-    score = my_font.render(f"{score_point}", True, black)
+    # score count
+    score = my_font.render(f"{current_score_point}", True, black)
     score_rect = score.get_rect()
     score_rect.center = (30, 22)
 
-    life_count = my_font.render(f"{life_point}", True, black)
+    # life count
+    life_count = my_font.render(f"{current_life_point}", True, black)
     life_count_rect = life_count.get_rect()
     life_count_rect.center = (110, 22)
 
@@ -131,6 +195,7 @@ while run:
     SCREEN.blit(score, score_rect)
     SCREEN.blit(life_count, life_count_rect)
     
+    
     # keys get pressed
     keys = pygame.key.get_pressed()
 
@@ -138,6 +203,8 @@ while run:
     falling_life_point()
 
     falling_bugs()
+
+    end_game()
     
    
     # fish movement
@@ -156,6 +223,10 @@ while run:
         if event.type == pygame.QUIT:
             run = False
             sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_ESCAPE:
+                pygame.cdrom.get_paused()
     
 
     # slowdown cycle
